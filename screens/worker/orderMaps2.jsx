551a8f35f03d776a4a6baca2ead28fd3d02b1506
xX-Higgs-Xx <20223tn131@utez.edu.mx>
2024-03-17 03:40:32 -0600

    Eliminacion de pantalla por cambio de nombre al archivo

---
+++ screens/worker/orderMaps2.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const orderMaps2 = () => {
+  return (
+    <View>
+      <Text>orderMaps2</Text>
+    </View>
+  )
+}
+
+export default orderMaps2
